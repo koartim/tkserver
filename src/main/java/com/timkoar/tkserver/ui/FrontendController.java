6e867da12f4cd617d7253383bf29c05b8336146f
Timothy Koar <koartimothy@gmail.com>
2024-11-13 12:27:42 -0500

    fixing ui controller

--- src/main/java/com/timkoar/tkserver/ui/FrontendController.java
+++ src/main/java/com/timkoar/tkserver/ui/FrontendController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FrontendController {
     @RequestMapping(value = { "/", "/contact", "/about", "/blog", "/**/{[path:[^\\.]*]}" })
     public String forward() {
+        System.out.println("jere");
         return "forward:/index.html";
     }
 }
